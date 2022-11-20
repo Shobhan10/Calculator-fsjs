30c03aca716b37f72fa74ee90984ff6ba56aa842
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-11-20 21:08:59 +0530

    added insert clean and back functionalities

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+// const buttons = document.querySelectorAll(".button");
+// // console.log(buttons);
+// for (let i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener("click", () => {
+//     console.log(buttons[i].value);
+//   });
+// }
 
-function insert(num){
-  }
-  
-  function equals(){
-    
-      
-  }
-  
-  function clean(){
-  }
-  
-  function back(){
-  }
+// const b = document.getElementById("b");
+// console.log(b);
+
+function insert(num) {
+  console.log(num)
+  const show = document.querySelector(".textview");
+  show.value += num
+}
+
+function equals() {}
+
+function clean() {
+  const show = document.querySelector(".textview");
+  show.value = "";
+}
+
+function back() {
+  const show = document.querySelector(".textview");
+  show.value = show.value.slice(0, -1);
+}
